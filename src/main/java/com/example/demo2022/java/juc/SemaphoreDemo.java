1f2d267d1bcb908395dd25b19e2c644a55fc1b14
gaoyuanyuan2 <823789010@qq.com>
2022-12-08 23:30:35 +0800

    update

--- src/main/java/com/example/demo2022/java/juc/SemaphoreDemo.java
+++ src/main/java/com/example/demo2022/java/juc/SemaphoreDemo.java
@@ -9,6 +9,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 
 public class SemaphoreDemo {
     public static void main(String[] args) throws InterruptedException {
