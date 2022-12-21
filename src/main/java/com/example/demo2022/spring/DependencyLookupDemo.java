2d1adadc11d69fe73a62a385c0d39cdd06de91a9
yanguowei <6516@vetech.cn>
2022-12-21 21:27:08 +0800

    延时查找

---
+++ src/main/java/com/example/demo2022/spring/DependencyLookupDemo.java
@@ -0,0 +1,26 @@
+package com.example.demo2022.spring;
+
+import com.example.demo2022.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 依赖查找
+ *
+ * @author : Y
+ * @since 2022/12/21 21:23
+ */
+public class DependencyLookupDemo {
+    public static void main(String[] args) {
+        // 配置 XML 配置文件
+        // 启动 Spring 应用上下文
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context.xml");
+        lookupInLazy(beanFactory);
+    }
+    private static void lookupInLazy(BeanFactory beanFactory) {
+        ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
+        User user = objectFactory.getObject();
+        System.out.println("延迟查找：" + user);
+    }
+}
