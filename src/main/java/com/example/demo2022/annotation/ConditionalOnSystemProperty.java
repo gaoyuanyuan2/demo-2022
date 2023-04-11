16a7c7708645fd56fdf145cebf4becee97eab9de
yanguowei <6516@vetech.cn>
2023-04-11 20:32:55 +0800

    添加装配

---
+++ src/main/java/com/example/demo2022/annotation/ConditionalOnSystemProperty.java
@@ -0,0 +1,36 @@
+package com.example.demo2022.annotation;
+
+
+import com.example.demo2022.spring.condition.OnSystemPropertyCondition;
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Java 系统属性 条件判断
+ *
+ * @author 小马哥
+ * @since 2018/5/15
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Documented
+@Conditional(OnSystemPropertyCondition.class)
+public @interface ConditionalOnSystemProperty {
+
+    /**
+     * Java 系统属性名称
+     * @return
+     */
+    String name();
+
+    /**
+     * Java 系统属性值
+     * @return
+     */
+    String value();
+}
